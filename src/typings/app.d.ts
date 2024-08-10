7a255da9fa0324e5879a7891a32a99b932804eb2
xlsea <m@xlsea.cn>
2024-08-10 20:05:21 +0800

    fix: 修复问候语多语言

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -612,6 +612,14 @@ declare namespace App {
           devDep: string;
         };
         home: {
+          Greeting: string;
+          morningGreeting: string;
+          bthGreeting: string;
+          noonGreeting: string;
+          athGreeting: string;
+          duskGreeting: string;
+          eveningGreeting: string;
+          earlyMorningGreeting: string;
           retryTaskCount: string;
           jobTaskCount: string;
           userCount: string;
